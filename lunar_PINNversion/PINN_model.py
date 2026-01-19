035296417e642401d88bc0e87b7f00f1a706aeac
Deyan Mihaylov <d.p.mihaylov@gmail.com>
2026-01-18 21:33:04 -0500

    Fix linting

--- lunar_PINNversion/PINN_model.py
+++ lunar_PINNversion/PINN_model.py
@@ -7,23 +7,32 @@ import matplotlib.colors as colors
 from torch.optim.lr_scheduler import ExponentialLR
 
 if torch.cuda.is_available():
-    device = torch.device("cuda")  # Select GPU
+    device = torch.device("cuda") # Select GPU
     print(f"Using GPU: {torch.cuda.get_device_name(0)}")
 else:
-    device = torch.device("cpu")  # Fallback to CPU
+    device = torch.device("cpu") # Fallback to CPU
     print("Using CPU")
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, in_dim=3, num_frequencies=4, base_freq = 0.98):
+    def __init__(
+        self,
+        in_dim=3,
+        num_frequencies=4,
+        base_freq=0.98,
+    ):
         super().__init__()
         self.in_dim = in_dim
         self.num_frequencies = num_frequencies
         freq_bands = base_freq ** torch.arange(num_frequencies)
         print(f"freq bands are: {freq_bands}")
         self.register_buffer("freq_bands", freq_bands)
-    def forward(self, x):
+
+    def forward(
+        self,
+        x,
+    ):
         """
-        x: (N,3)
+        x: (N, 3)
         """
         enc = [x]
         for f in self.freq_bands:
@@ -34,17 +43,25 @@ class PositionalEncoding(nn.Module):
     def out_dim(self):
         return self.in_dim * (1 + 2 * self.num_frequencies)
 
-
     def out_dim(self):
         return self.in_dim * (1 + 2 * self.num_frequencies)
 
+
 class PINN(nn.Module):
-    def __init__(self, pe_num_freqs =6, base_freq = 1.3,
-                 layers=[64,128,128,128]):
+    def __init__(
+        self,
+        pe_num_freqs=6,
+        base_freq=1.3,
+        layers=[64, 128, 128, 128],
+    ):
         super().__init__()
         # in_dim = 3
 
-        self.pe = PositionalEncoding(in_dim=3, num_frequencies=pe_num_freqs, base_freq=base_freq)
+        self.pe = PositionalEncoding(
+            in_dim=3,
+            num_frequencies=pe_num_freqs,
+            base_freq=base_freq,
+        )
 
         in_dim = self.pe.out_dim()
 
@@ -57,7 +74,10 @@ class PINN(nn.Module):
                 net.append(nn.Tanh())
         self.net = nn.Sequential(*net)
 
-    def forward(self, xyz):
+    def forward(
+        self,
+        xyz,
+    ):
         xyz_pe = self.pe.forward(xyz)
         return self.net(xyz_pe)
         # return self.net(xyz)
@@ -66,13 +86,22 @@ class PINN(nn.Module):
         xyz.requires_grad_(True)
         phi = model(xyz)
 
-        grad = torch.autograd.grad(phi, xyz, torch.ones_like(phi), create_graph=True)[0]
+        grad = torch.autograd.grad(
+            phi, xyz, torch.ones_like(phi), create_graph=True,
+        )[0]
+
         phix, phiy, phiz = grad[:, 0:1], grad[:, 1:2], grad[:, 2:3]
 
-        phixx = torch.autograd.grad(phix, xyz, torch.ones_like(phix), create_graph=True)[0][:, 0:1]
-        phiyy = torch.autograd.grad(phiy, xyz, torch.ones_like(phiy), create_graph=True)[0][:, 1:2]
-        phizz = torch.autograd.grad(phiz, xyz, torch.ones_like(phiz), create_graph=True)[0][:, 2:3]
+        phixx = torch.autograd.grad(
+            phix, xyz, torch.ones_like(phix), create_graph=True,
+        )[0][:, 0:1]
+
+        phiyy = torch.autograd.grad(
+            phiy, xyz, torch.ones_like(phiy), create_graph=True,
+        )[0][:, 1:2]
+
+        phizz = torch.autograd.grad(
+            phiz, xyz, torch.ones_like(phiz), create_graph=True,
+        )[0][:, 2:3]
 
         return phixx + phiyy + phizz
-
-
